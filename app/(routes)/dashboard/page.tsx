dc2af9145086f01191801b0e904dbdc3183513cf
princesinghrajput <psr8084@gmail.com>
2024-09-07 17:05:48 +0530

    implemnted protected routes

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,8 +1,15 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs'
 import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className='text-black'>page</div>
+    <div className='text-black'>
+        <Button>
+          <LogoutLink>Logout</LogoutLink>
+        </Button>
+    </div>
   )
 }
 
